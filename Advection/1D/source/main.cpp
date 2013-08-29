dab77209a7c055cf07d525f5e6ceb8941eb3c613
Ted Studley <TedStudley@Gmail.com>
2013-08-29 12:55:28 -0700

    Updated README.md to have information on functions and running the program. Stay tuned for output/gnuplot/plotscript

--- Advection/1D/source/main.cpp
+++ Advection/1D/source/main.cpp
@@ -10,15 +10,16 @@ using namespace std;
 
 int main() {
   const unsigned int N = 1024;
-  const double t0      = 0.00001;
   const double delta_t = 0.0001;
   const double v       = 1.0;
 
   VectorXd u (N);
 
-  fourierSquare (u, N, t0);
+  squareWave (u, N);
 
-  frommVanLeer (u, N, v, delta_t);
+  for (int timestep = 0; timestep < 10000; ++timestep) {
+    frommMethod (u, N, v, delta_t);
+  }
 
   cout << u.transpose() << endl;
 
